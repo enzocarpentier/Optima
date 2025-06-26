321e47103c758f77fa7e5274d0659c4a3b55de2f
enzocarpentier <enzo.carpentier@univ-rouen.fr>
2025-06-26 13:01:19 +0200

    Initial Commit

---
+++ Optima/OptimaApp.swift
@@ -0,0 +1,17 @@
+//
+//  OptimaApp.swift
+//  Optima
+//
+//  Created by Enzo Carpentier on 26/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct OptimaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
